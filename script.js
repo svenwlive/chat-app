611c6fd9d24ed4e73e0e342c66ff03a4361526fc
Svenw <74722783+svenwlive@users.noreply.github.com>
2024-11-22 09:42:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ if (username != null) {
 }
 
 async function getMessages() {
-    const url = 'https://svenwlive.github.io/chat-app/api/chat/store.json';
+    const url = 'https://svenwlive.github.io/api-chat/db.json';
     const response = await fetch(url);
     const json = await response.json();
     const element = document.querySelector('.messages');
@@ -38,7 +38,7 @@ textbox.addEventListener("keydown", function (event) {
   // console.log(`User input: ${event.target.value}`);
   if (event.key === "Enter") {
     // console.info("sending message")
-    fetch('https://svenwlive.github.io/chat-app/api/chat/store.json', {
+    fetch('https://svenwlive.github.io/api-chat/db.json', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
